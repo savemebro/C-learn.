e2a409ee08a2f411b72093c15d70a8a7a1ffb483
Save_my_Life <novikov_ggg@bk.ru>
2023-02-01 14:20:58 +0300

    example_005 сделали авторизацию на имя

---
+++ Example_005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя!");
+string username = Console.ReadLine();
+
+if ( username.ToLower() == "маша")
+{
+    Console.Write ( "я тебя люблю!" );
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
